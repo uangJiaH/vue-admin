8ad891f5e30d310a7e9309bc96794212b0e0989c
huangjia <2718932137@qq.com>
2022-07-05 09:37:58 +0800

    feat(git): 创建项目的基础配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus).use(store).use(router).mount('#app')
